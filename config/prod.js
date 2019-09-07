9af90998199aabdb64fc9f03d59f8506779eeef5
MaxFurman <furmanmax96@gmail.com>
2019-09-07 11:32:40 +0300

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 //prod keys
 module.exports = {
-    googleClientId: procces.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: procces.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: procces.env.MONGO_URI,
-    cookieKey: procces.env.COOKIE_KEY
+    googleClientId: proccess.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: proccess.env.MONGO_URI,
+    cookieKey: proccess.env.COOKIE_KEY
 }
